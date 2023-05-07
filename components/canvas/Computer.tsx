0ad7846c616ce82141fc4cb884b9858968dea1aa
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-07 03:58:00 -0400

    Refactor hero section and add media query

--- components/canvas/Computer.tsx
+++ components/canvas/Computer.tsx
@@ -1,27 +1,50 @@
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import CanvasLoader from './loader';
 
-export function Computers() {
+export function Computers({ isMobile }: { isMobile: boolean }) {
   const computer = useGLTF('/desktop_pc/scene.gltf');
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <pointLight intensity={1} />
       <spotLight intensity={1} position={[-20, 50, 10]} angle={0.12} penumbra={1} shadow-mapSize={1024} castShadow />
-      <primitive object={computer.scene} scale={0.75} position={[0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [0, -2, -1] : [0, -2.2, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 }
 
 export default function ComputerCanvas() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const meediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(meediaQuery.matches);
+
+    const handleMediaQueryChange = (e: MediaQueryListEvent) => {
+      setIsMobile(e.matches);
+    };
+
+    meediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      meediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 20 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,30 +1,51 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
 import { cn } from '@/lib/utils';
 
-// import {motion} from 'framer-motion'
 import { ComputerCanvas } from './canvas';
 
 export default function Hero() {
   return (
-    <section className='relative mx-auto h-screen w-full'>
-      <div className='absolute inset-0 top-[120px] mx-auto flex max-w-7xl flex-row items-start gap-5 px-6 sm:px-16'>
-        <div className='mt-5 flex flex-col items-center justify-center'>
-          <div className='h-5 w-5 rounded-full bg-[#915eff]' />
-          <div className='violet-gradient h-40 w-1 sm:h-80' />
-        </div>
+    console.log('Hero'),
+    (
+      <section className='relative mx-auto h-screen w-full'>
+        <div className='paddingX absolute inset-0 top-[120px] mx-auto flex max-w-7xl flex-row items-start gap-5 px-6 sm:px-16'>
+          <div className='mt-5 flex flex-col items-center justify-center'>
+            <div className='h-5 w-5 rounded-full bg-[#915eff]' />
+            <div className='violet-gradient h-40 w-1 sm:h-80' />
+          </div>
 
-        <div>
-          <h1 className='heroHeadText'>
-            Hi, I&apos;m <span className='text-[#915eff]'>Ismael</span>
-          </h1>
-          <p className={cn('heroSubText text-white-100 mt-2')}>
-            I&apos;m a full-stack developer and designer. <br className='hidden sm:block' /> I love to build things that
-            make people happy.
-          </p>
+          <div>
+            <h1 className='heroHeadText'>
+              Hi, I&apos;m <span className='text-[#915eff]'>Ismael</span>
+            </h1>
+            <p className={cn('heroSubText text-white-100 mt-2')}>
+              I&apos;m a full-stack developer and designer. <br className='hidden sm:block' /> I love to build things
+              that make people happy.
+            </p>
+          </div>
         </div>
-      </div>
-      <ComputerCanvas />
-    </section>
+        <ComputerCanvas />
+        <div className='xs:bottom-10 absolute bottom-32 flex w-full items-center justify-center'>
+          <a href='#about'>
+            <div className='border-secondary flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 p-2'>
+              <motion.div
+                animate={{
+                  y: [0, 24, 0],
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: 'loop',
+                }}
+                className='bg-secondary mb-1 h-3 w-3 rounded-full'
+              />
+            </div>
+          </a>
+        </div>
+      </section>
+    )
   );
 }
