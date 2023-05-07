bba45a74116d8283427df3f85c5fa762a35a2ebb
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-07 03:31:33 -0400

    Add loading percentage to computer canvas

--- components/canvas/Computer.tsx
+++ components/canvas/Computer.tsx
@@ -2,6 +2,8 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 
+import CanvasLoader from './loader';
+
 export function Computers() {
   const computer = useGLTF('/desktop_pc/scene.gltf');
   return (
@@ -17,7 +19,7 @@ export function Computers() {
 export default function ComputerCanvas() {
   return (
     <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 20 }} gl={{ preserveDrawingBuffer: true }}>
-      <Suspense>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Computers />
       </Suspense>
